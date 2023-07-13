7b3eea637317c156b4bbdcdf2864b9d00860dff7
Rishav Sah <rishavsah4u@gmail.com>
2023-07-13 16:06:13 +0545

    problem 1

--- problem-1.js
+++ problem-1.js
@@ -1,1 +1,16 @@
-// commit check
+// Write a function that takes an array of numbers as input and returns the sum of all the positive numbers in the array.
+
+const listOfNumbers = [10, -20, 40, 50];
+let sumOfNumbers = 0;
+
+function sumOfArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      const element = arr[i];
+      sumOfNumbers += element;
+    }
+  };
+  return sumOfNumbers;
+};
+
+console.log(sumOfArray(listOfNumbers));
